6ee2e27a2e09c9d78817a282af07926996e7ffab
Jared <35243598+Jbranscum2015@users.noreply.github.com>
2021-05-12 19:14:04 -0400

    React App is staged for developing the components. Integrated vendor resources and template resources from iPortfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. imagine thiss smurf, test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="colorlib-page">
+      <div id="container-wrap">
+	  {/* <Sidebar></Sidebar>
+		  <div id="colorlib-main">
+            Test
+            <Introduction></Introduction>
+            <About></About>
+	  <Timeline></Timeline>
+	  </div>*/}
+      </div>
     </div>
   );
 }

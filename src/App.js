2aae6aa1180d49ac4d9c4ca2c3baad54ea7d3a80
Jared <35243598+Jbranscum2015@users.noreply.github.com>
2021-05-12 02:07:19 -0400

    travis-ci integration to automate npm deploy on commits to main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. imagine thiss smurf
         </p>
         <a
           className="App-link"

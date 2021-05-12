de7bb51b07f4e76a4ea25d0d1b20e57eb11560c8
Jared <35243598+Jbranscum2015@users.noreply.github.com>
2021-05-12 02:10:16 -0400

    second test to trigger travis ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. imagine thiss smurf
+          Edit <code>src/App.js</code> and save to reload. imagine thiss smurf, test
         </p>
         <a
           className="App-link"

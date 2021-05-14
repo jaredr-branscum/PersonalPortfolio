61d978dca7af60fc623c0cc52f2d32d403b756f4
Jared <35243598+Jbranscum2015@users.noreply.github.com>
2021-05-13 22:36:59 -0400

    changed the scroll to top arrow to point to hero div

--- src/App.js
+++ src/App.js
@@ -542,7 +542,7 @@ function App() {
       </div>
     </div>
   </footer>{/* End  Footer */}
-  <a href="/#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short" /></a>
+  <a href="#hero" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short" /></a>
   </div>
   );
 }
